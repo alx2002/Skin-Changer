6d258293a4ef974bf9d10368d0c6fc8a643ce9b8
Alexander Gaiduk <alexandergy108@gmail.com>
2019-05-10 19:32:47 -0700

    Small Changes

--- reskin.cs
+++ reskin.cs
@@ -10,6 +10,7 @@ using Lib_K_Relay.Utilities;
 using System.Linq;
 using Helper;
 using System.Net;
+using System.Diagnostics;
 
 namespace SkinSelector
 {
@@ -18,26 +19,28 @@ namespace SkinSelector
         public string GetAuthor() => "CD";
         public string GetName() => "Reskin changer";
         public string GetDescription() => "Reskin in any map, limitation is the skins you own, you can find your skinIDs from https://static.drips.pw/rotmg/production/current/json/Objects.json";
-        public string[] GetCommands() => new string[] { "[/reskin Id1 Id2 Id3]", "[/skin Id]","[/use] displays command list" };
+        public string[] GetCommands() => new string[] { "[/reskin Id1 Id2 Id3]", "[/skin Id]", "[/use] displays command list" };
         public void Initialize(Proxy proxy)
         {
             proxy.HookCommand("reskin", ChangeSkin);
             proxy.HookCommand("skin", Skin);
             proxy.HookCommand("use", Helper);
         }
-        private const int Timeout = 2000;
-        private bool enabled;
+        private const short Timeout = 2000;
+        bool enabled;
         private void ChangeSkin(Client client, string command, string[] args)
         {
             ReskinPacket reskin = (ReskinPacket)Packet.Create(PacketType.RESKIN);
-            Int32 pd = 8000;
+            short pd = 8000; //up this if server very laggy
             try
             {
+                client.Notify("How many skins do you want to switch?");
+                int iterateskin = int.Parse(args[0]); 
                 enabled = true;
             Label_0000:
                 PluginUtils.Delay(pd, delegate
                 {
-                    reskin.SkinId = int.Parse(args[0]);
+                    reskin.SkinId = int.Parse(args[iterateskin]); //iterate new array string by input
                     client.SendToServer(reskin);
                 });
                 Thread.Sleep(Timeout);
@@ -49,24 +52,12 @@ namespace SkinSelector
                 Thread.Sleep(Timeout);
                 PluginUtils.Delay(pd, delegate
                 {
-                        reskin.SkinId = int.Parse(args[2]);
-                        client.SendToServer(reskin);
-                });
-                Thread.Sleep(Timeout);
-                PluginUtils.Delay(pd, delegate
-                {
-                    reskin.SkinId = int.Parse(args[3]);
-                    client.SendToServer(reskin);
-                });
-                Thread.Sleep(Timeout);
-                PluginUtils.Delay(pd, delegate
-                {
-                    reskin.SkinId = int.Parse(args[4]);
+                    reskin.SkinId = int.Parse(args[2]);
                     client.SendToServer(reskin);
                 });
                 Thread.Sleep(Timeout);
                 goto Label_0000;
-                }
+            }
             catch (Exception e)
             {
                 enabled = false;
@@ -102,13 +93,26 @@ namespace SkinSelector
         }
         private void Helper(Client client, string command, string[] args)
         {
+            string url = "https://static.drips.pw/rotmg/production/current/json/Objects.json";
             try
             {
                 using (var cl = new WebClient())
                 {
-                    cl.DownloadFile("https://static.drips.pw/rotmg/production/current/json/Objects.json", "Objects.json");
+                    try
+                    {
+                        cl.DownloadFile("url", "Objects.json");
+                    }
+                    catch(Exception q)
+                    {
+                        client.Notify("Download Failed!\n You can do it yourself from:"+url);
+                    }
                     client.Notify("Download Completed! Find skins Ids in Objects.Json!");
-                    System.Diagnostics.Process.Start("Objects.json");
+                    try
+                    {Process.Start("Objects.json");}
+                    catch (Exception q)
+                    {
+                        client.Notify(q.ToString()+"       Opening failed");
+                    }
                 };
             }
             catch (Exception e)
@@ -117,8 +121,8 @@ namespace SkinSelector
                 Console.WriteLine(e);
 #endif
             }
-            if (args?.Any() == true || (args.Length == 0)) 
-            { 
+            if (args?.Any() == true || (args.Length == 0))
+            {
                 client.Notify("[/reskin] resets to original skin!");
                 client.Notify("[/skin ID] aka skinType)");
                 client.Notify("[/reskin ID1 ID2 ID3] (this may cause unstable gameplay!)");
